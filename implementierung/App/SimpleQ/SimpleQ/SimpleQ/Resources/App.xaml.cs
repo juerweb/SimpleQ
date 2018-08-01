27b12ca29ff4b8bb138c9ae235f175a1cefad2f8
Jürgen Weber <i14067@student.htlwrn.ac.at>
2018-08-01 13:18:33 +0200

    Projekt erstellt und Standarddateien eingefügt.

---
+++ implementierung/App/SimpleQ/SimpleQ/SimpleQ/Resources/App.xaml.cs
@@ -0,0 +1,33 @@
+using SimpleQ.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+namespace SimpleQ
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new Main();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
