b5ef0d1dc6133d92570e23325f2750b5c94f743e
Tobias Krukenfellner <i14057@student.htlwrn.ac.at>
2018-09-12 14:13:05 +0200

    --

---
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Scripts/surveyResults.js
@@ -0,0 +1,157 @@
+﻿function switchToMulti() {
+    $("#trendBtn").removeClass("btn-primary").addClass("btn-secondary");
+    $("#singleBtn").removeClass("btn-secondary").addClass("btn-primary");
+    $("#trendBtn").attr("disabled", true);
+    $("#singleBtn").attr("disabled", false);
+    $("#singleResultPartial").hide();
+    $("#multiResultPartial").show();
+}
+
+function switchToSingle() {
+    $("#singleBtn").removeClass("btn-primary").addClass("btn-secondary");
+    $("#trendBtn").removeClass("btn-secondary").addClass("btn-primary");
+    $("#singleBtn").attr("disabled", true);
+    $("#trendBtn").attr("disabled", false);
+    $("#multiResultPartial").hide();
+    $("#singleResultPartial").show();
+}
+
+function changeChart1Multi() {
+    var ctx = document.getElementById("chart1Multi").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: document.getElementById("chart1SelectMulti").value,
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [113, 192, 333, 45, 112, 23],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+function changeChart2Multi() {
+    var ctx2 = document.getElementById("chart2Multi").getContext('2d');
+    var myChart2 = new Chart(ctx2, {
+        type: document.getElementById("chart2SelectMulti").value,
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    });
+}
+
+function changeChart1Single() {
+    var ctx = document.getElementById("chart1Single").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: document.getElementById("chart1SelectSingle").value,
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [113, 192, 333, 45, 112, 23],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+}
+function changeChart2Single() {
+    var ctx2 = document.getElementById("chart2Single").getContext('2d');
+    var myChart2 = new Chart(ctx2, {
+        type: document.getElementById("chart2SelectSingle").value,
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        }
+    });
+}
