ed8c290ba3601b62c47323dff7b04055e9c1ace6
Tobias Krukenfellner <tkruk34@gmail.com>
2018-09-13 18:33:14 +0200

    --

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Scripts/surveyResults.js
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Scripts/surveyResults.js
@@ -20,28 +20,24 @@ function changeChart1Multi() {
     document.getElementById("chart1MultiCol").innerHTML = '<canvas id="chart1Multi" width="800" height="500"></canvas>';
     var canvas = document.getElementById("chart1Multi");
     var ctx = canvas.getContext('2d');
+
     var myChart = new Chart(ctx, {
-        type: document.getElementById("chart1SelectMulti").value,
+        type: 'line', /*document.getElementById("chart1SelectMulti").value,*/
         data: {
             labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
             datasets: [{
                 label: '# of Votes',
                 data: [113, 192, 333, 45, 112, 23],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
+
                 borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(54, 162, 235, 1)'
+                ],
+                borderWidth: 1
+            }, {
+                label: '# of Votes',
+                data: [130, 80, 200, 112, 112, 200],
+                borderColor: [
+                    'rgba(255,99,132,1)'
                 ],
                 borderWidth: 1
             }]
