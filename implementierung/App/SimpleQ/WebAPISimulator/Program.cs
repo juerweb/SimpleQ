6f9bb3ba8f0137fca53c8cab16b7246c146845d5
Jürgen Weber <i14067@student.htlwrn.ac.at>
2018-09-15 13:47:57 +0200

    SharedClasses hinzugefügt und passende Klassen eingefügt.

---
+++ implementierung/App/SimpleQ/WebAPISimulator/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace WebAPISimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HTTP Server wird gestartet...");
+
+            HttpListener httpListener = new HttpListener();
+
+            httpListener.Prefixes.Add("http://localhost:1899/test/");
+
+            httpListener.Start();
+
+            new Thread(() => {
+                HandleListener(httpListener);
+            }).Start();
+        }
+
+        private static void HandleListener(HttpListener httpListener)
+        {
+            while (true)
+            {
+                HttpListenerContext context = httpListener.GetContext();
+
+                new Thread(() => {
+                    HandleRequest(context);
+                }).Start();
+            }
+
+        }
+
+        private static void HandleRequest(HttpListenerContext context)
+        {
+
+            Console.WriteLine("Handle new Connection...");
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+
+            Uri link = request.Url;
+
+            switch (link.AbsolutePath)
+            {
+                case "Register":
+                    RegistrationData
+
+
+                    response.ContentType = "text/json";
+                    response.StatusCode = 200;
+                    response.StatusDescription = "OK";
+                    StreamWriter writer = new StreamWriter(response.OutputStream);
+                    writer.WriteLine("<http><body>Test</body></http>");
+                    writer.Close();
+                    break;
+                case "Unregister":
+                    break;
+                case "AnswerSurvey":
+                    break;
+            }
+            response.Close();
+        }
+    }
+}
