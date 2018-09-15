08f1c2f1d581d33d6d1c860775bd25c7184cb7bf
Jürgen Weber <i14067@student.htlwrn.ac.at>
2018-09-15 13:57:43 +0200

    Shared Classes hinzugefügt.

---
+++ implementierung/SharedClasses/SharedClasses/RegistrationData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleQ.Shared
+{
+    public class RegistrationData
+    {
+        public string CustCode { get; set; }
+        public int PersId { get; set; }
+        public int DepId { get; set; }
+        public string DepName { get; set; }
+    }
+}
