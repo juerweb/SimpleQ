1025afd3d481b64ea315f4732ac9c8e1a599b095
Tobias Krukenfellner <tkruk34@gmail.com>
2018-10-08 16:47:07 +0200

    --

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Controllers/SurveyCreationController.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Controllers/SurveyCreationController.cs
@@ -16,43 +16,43 @@ namespace SimpleQ.Webinterface.Controllers
         [HttpPost]
         public ActionResult New(SurveyCreationModel s)
         {
-            using (var db = new SimpleQDBEntities())
-            {
-                s.Survey.CustCode = CustCode;
-                //s.Survey.EndDate = s.Survey.StartDate.AddDays(7);
-                db.Surveys.Add(s.Survey);
-                s.SelectedDepartments.ForEach(d =>
-                {
-                    db.Askings.Add(new Asking { SvyId = s.Survey.SvyId, DepId = d, CustCode = CustCode });
-                });
-                db.SaveChanges();
-            }
+            //using (var db = new SimpleQDBEntities())
+            //{
+            //    s.Survey.CustCode = CustCode;
+            //    //s.Survey.EndDate = s.Survey.StartDate.AddDays(7);
+            //    db.Surveys.Add(s.Survey);
+            //    s.SelectedDepartments.ForEach(d =>
+            //    {
+            //        db.Askings.Add(new Asking { SvyId = s.Survey.SvyId, DepId = d, CustCode = CustCode });
+            //    });
+            //    db.SaveChanges();
+            //}
 
-            HostingEnvironment.QueueBackgroundWorkItem(ct =>
-            {
-                Thread.Sleep(s.Survey.StartDate - DateTime.Now);
+            //HostingEnvironment.QueueBackgroundWorkItem(ct =>
+            //{
+            //    Thread.Sleep(s.Survey.StartDate - DateTime.Now);
 
-                using (var db = new SimpleQDBEntities())
-                {
-                    // Gesamtanzahl an Personen von allen ausgewählten Abteilungen ermitteln
-                    int totalPersons = db.Departments
-                        .Where(d => s.SelectedDepartments.Contains(d.DepId) && d.CustCode == CustCode)
-                        .Sum(d => d.AskedPersons.Count);
+            //    using (var db = new SimpleQDBEntities())
+            //    {
+            //        // Gesamtanzahl an Personen von allen ausgewählten Abteilungen ermitteln
+            //        int totalPersons = db.Departments
+            //            .Where(d => s.SelectedDepartments.Contains(d.DepId) && d.CustCode == CustCode)
+            //            .Sum(d => d.AskedPersons.Count);
 
-                    s.SelectedDepartments.ForEach(d =>
-                    {
-                        // Anzahl an Personen in der aktuellen Abteilung (mit DepId = d)
-                        int currPersons = db.Departments
-                            .Where(dep => dep.DepId == d && dep.CustCode == CustCode)
-                            .Select(dep => dep.AskedPersons.Count).First();
+            //        s.SelectedDepartments.ForEach(d =>
+            //        {
+            //            // Anzahl an Personen in der aktuellen Abteilung (mit DepId = d)
+            //            int currPersons = db.Departments
+            //                .Where(dep => dep.DepId == d && dep.CustCode == CustCode)
+            //                .Select(dep => dep.AskedPersons.Count).First();
 
-                        // GEWICHTETE Anzahl an zu befragenden Personen in der aktuellen Abteilung
-                        int toAsk = s.Amount * (int)Math.Round(currPersons / (double)totalPersons);
+            //            // GEWICHTETE Anzahl an zu befragenden Personen in der aktuellen Abteilung
+            //            int toAsk = s.Amount * (int)Math.Round(currPersons / (double)totalPersons);
 
-                        SimpleQHub.SendSurvey(d, toAsk, CustCode, s.Survey);
-                    });
-                }
-            });
+            //            SimpleQHub.SendSurvey(d, toAsk, CustCode, s.Survey);
+            //        });
+            //    }
+            //});
 
             return RedirectToAction("Index", "Home");
         }
