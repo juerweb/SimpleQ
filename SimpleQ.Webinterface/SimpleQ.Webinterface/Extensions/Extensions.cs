7a1cefd56082ec7e0155e8dd4e727e00fd1fd1ff
Lukas Schendlinger <i14061@student.htlwrn.ac.at>
2018-10-09 10:40:15 +0200

    webinterface from feature/ls/webinterface5

---
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Extensions/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace SimpleQ.Webinterface.Extensions
+{
+    public static class Extensions
+    {
+        public static byte[] GetSHA512(this string str)
+        {
+            using (var alg = SHA512.Create())
+            {
+                return alg.ComputeHash(Encoding.UTF8.GetBytes(str));
+            }
+        }
+    }
+}
