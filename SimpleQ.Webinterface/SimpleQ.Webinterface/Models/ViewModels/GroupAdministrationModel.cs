db4da1edec8562a44fbb84398601206a751c9e36
Tobias Krukenfellner <i14057@student.htlwrn.ac.at>
2018-11-06 13:12:22 +0100

    --

---
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Models/ViewModels/GroupAdministrationModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleQ.Webinterface.Models.ViewModels
+{
+    public class GroupAdministrationModel
+    {
+        #region Controller to View
+        public List<Department> Departments { get; set; }
+        #endregion
+
+        #region View to Controller
+        #endregion
+    }
+}
