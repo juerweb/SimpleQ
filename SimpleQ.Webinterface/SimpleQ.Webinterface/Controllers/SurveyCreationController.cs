ecce885f3dff93f6540472863ca7c69e9f02f244
Tobias Krukenfellner <i14057@student.htlwrn.ac.at>
2018-11-07 07:56:38 +0100

    --

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Controllers/SurveyCreationController.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Controllers/SurveyCreationController.cs
@@ -63,7 +63,7 @@ namespace SimpleQ.Webinterface.Controllers
         {
             using (var db = new SimpleQDBEntities())
             {
-                return Json(db.Surveys.Where(s => s.SvyId == svyId && s.CustCode == CustCode && s.Template).FirstOrDefault());
+                return Json(db.Surveys.Where(s => s.SvyId == svyId && s.CustCode == CustCode && s.Template).FirstOrDefault(),JsonRequestBehavior.AllowGet);
             }
         }
 
