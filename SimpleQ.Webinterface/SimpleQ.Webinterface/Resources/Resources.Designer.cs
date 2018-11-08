4880dd6efd7821da437a65b4d293478760b2bae9
Tobias Krukenfellner <i14057@student.htlwrn.ac.at>
2018-11-08 20:08:02 +0100

    --

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Resources/Resources.Designer.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Resources/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace SimpleQ.Webinterface.App_LocalResources {
+namespace SimpleQ.Webinterface.Resources {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace SimpleQ.Webinterface.App_LocalResources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SimpleQ.Webinterface.App_LocalResources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SimpleQ.Webinterface.Resources.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
