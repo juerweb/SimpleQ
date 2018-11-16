4787c98bf28be733b24e7b2b44a10c63a3c009ff
Tobias Krukenfellner <i14057@student.htlwrn.ac.at>
2018-11-16 21:08:48 +0100

    --

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Controllers/SurveyCreationController.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Controllers/SurveyCreationController.cs
@@ -64,7 +64,15 @@ namespace SimpleQ.Webinterface.Controllers
             using (var db = new SimpleQDBEntities())
             {
                 db.Configuration.LazyLoadingEnabled = false;
-                return Json(db.Surveys.Where(s => s.SvyId == svyId && s.CustCode == CustCode && s.Template).FirstOrDefault(), JsonRequestBehavior.AllowGet);
+                //List<Department> d = new List<Department>();
+                //d.AddRange(db.Surveys.Where(s => s.SvyId == svyId && s.CustCode == CustCode && s.Template).FirstOrDefault().Departments);
+                var json = Json(null);
+                json.RecursionLimit = 3;
+                json.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+                json.Data = db.Surveys.Where(s => s.SvyId == svyId && s.CustCode == CustCode && s.Template).FirstOrDefault();
+                //json.Data = Json(db.Surveys.Where(s => s.SvyId == svyId && s.CustCode == CustCode && s.Template).FirstOrDefault(), JsonRequestBehavior.AllowGet);
+                return json;
+                //return Json(d, JsonRequestBehavior.AllowGet);
             }
         }
 
