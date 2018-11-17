5b58b623c49f0c3d4a571dd4272d607a4f18f376
Lukas Schendlinger <lukas@schendlinger.com>
2018-11-17 17:35:54 +0100

    RegistrationData +CustName

--- implementierung/SharedClasses/SharedClasses/RegistrationData.cs
+++ implementierung/SharedClasses/SharedClasses/RegistrationData.cs
@@ -8,6 +8,7 @@ namespace SimpleQ.Shared
     public class RegistrationData
     {
         public string CustCode { get; set; }
+        public string CustName { get; set; }
         public int PersId { get; set; }
         public int DepId { get; set; }
         public string DepName { get; set; }
