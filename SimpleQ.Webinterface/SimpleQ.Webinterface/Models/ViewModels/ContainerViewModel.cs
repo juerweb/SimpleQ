250d57d691a91f6c0a686979fb0312d58475f6a8
Tobias Krukenfellner <tkruk34@gmail.com>
2018-11-18 16:44:52 +0100

    --

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Models/ViewModels/ContainerViewModel.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Models/ViewModels/ContainerViewModel.cs
@@ -10,6 +10,7 @@ namespace SimpleQ.Webinterface.Models.ViewModels
         public SurveyCreationModel SurveyCreationModel { get; set; }
         public SurveyResultsModel SurveyResultsModel { get; set; }
         public GroupAdministrationModel GroupAdministrationModel { get; set; }
-
+        public SettingsModel SettingsModel { get; set; }
+        public SupportModel SupportModel { get; set; }
     }
 }
