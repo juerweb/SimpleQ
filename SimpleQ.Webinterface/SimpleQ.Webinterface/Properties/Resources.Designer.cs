9c41de6d850861d63743cf5b301712eafcc9b758
Tobias Krukenfellner <tkruk34@gmail.com>
2018-11-20 21:43:42 +0100

    --

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Properties/Resources.Designer.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace SimpleQ.Webinterface.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SimpleQ.Webinterface.Resources.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("SimpleQ.Webinterface.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
