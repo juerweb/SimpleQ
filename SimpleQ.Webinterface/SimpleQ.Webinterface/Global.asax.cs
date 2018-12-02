7b3c65bcf6d367c46e0e72fbfd44569a599f6724
Tobias Krukenfellner <tkruk34@gmail.com>
2018-12-02 16:50:22 +0100

    mail sending implemented

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Global.asax.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Global.asax.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -20,6 +22,7 @@ namespace SimpleQ.Webinterface
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.NameIdentifier;
         }
     }
 }
