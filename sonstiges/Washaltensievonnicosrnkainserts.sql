88d0f017672eead6469b9d51b0f3c5cf1bc9c0db
Tobias Krukenfellner <tkruk34@gmail.com>
2018-12-05 21:26:04 +0100

    bugfixes surveyResults

---
+++ sonstiges/Washaltensievonnicosrnkainserts.sql
@@ -0,0 +1,31 @@
+use SimpleQDB
+go
+
+select * from Survey;
+
+update survey set Template = 1 where SvyId = 4;
+
+select * from AnswerOption;
+select * from Chooses;
+
+select * from Vote;
+
+insert into vote values(null)
+
+insert into Chooses values(47,30);
+insert into Chooses values(47,30);
+insert into Chooses values(49,30);
+insert into Chooses values(50,30);
+insert into Chooses values(51,31);
+insert into Chooses values(52,32);
+insert into Chooses values(53,32);
+insert into Chooses values(54,34);
+insert into Chooses values(55,34);
+insert into Chooses values(56,35);
+insert into Chooses values(57,36);
+insert into Chooses values(58,37);
+insert into Chooses values(59,37);
+insert into Chooses values(60,37);
+insert into Chooses values(61,41);
+insert into Chooses values(62,41);
+insert into Chooses values(63,41);
