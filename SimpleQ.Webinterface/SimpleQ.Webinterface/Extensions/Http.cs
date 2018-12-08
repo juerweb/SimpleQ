f562253ca013d2e4b54d98377e3a2080bd5027bd
Tobias Krukenfellner <i14057@student.htlwrn.ac.at>
2018-12-08 08:53:54 +0100

    --

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Extensions/Http.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Extensions/Http.cs
@@ -13,17 +13,20 @@ namespace SimpleQ.Webinterface.Extensions
 
         public static HttpStatusCodeResult BadRequest(string desc = null) => new HttpStatusCodeResult(400, desc);
 
-        public static HttpStatusCodeResult Unauthorized(string desc = null) => new HttpStatusCodeResult(401, desc);
+        public static HttpStatusCodeResult Forbidden(string desc = null) => new HttpStatusCodeResult(403, desc);
 
         public static HttpStatusCodeResult NotFound(string desc = null) => new HttpStatusCodeResult(404, desc);
 
         public static HttpStatusCodeResult Conflict(string desc = null) => new HttpStatusCodeResult(409, desc);
 
+        public static HttpStatusCodeResult InternalServerError(string desc = null) => new HttpStatusCodeResult(500, desc);
+
         public static HttpStatusCodeResult ServiceUnavailable(string desc = null) => new HttpStatusCodeResult(503, desc);
 
 
 
-        // Just a little joke ;)
+        // Just for fun
         public static HttpStatusCodeResult FourTwenty(string desc = null) => new HttpStatusCodeResult(420, "Get stoned");
+        public static HttpStatusCodeResult SixSixSix(string desc = null) => new HttpStatusCodeResult(666, "Possessed");
     }
 }
