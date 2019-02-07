81cccfa3e37641a1bf13f9d28663f4d06e3d3736
Lukas Schendlinger <i14061@student.htlwrn.ac.at>
2019-02-07 15:15:12 +0100

    Keep alive bugfix, bpmn rework

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Global.asax.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Global.asax.cs
@@ -13,16 +13,18 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Net;
+using System.Web.Hosting;
 
 namespace SimpleQ.Webinterface
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         private static Logger logger = LogManager.GetCurrentClassLogger();
-
         private static JobScheduler jobScheduler = new JobScheduler();
+        private static KeepAlive keepAlive;
 
-        protected void Application_Start()
+        protected void Application_Start(object sender, EventArgs e)
         {
             try
             {
@@ -39,11 +41,30 @@ namespace SimpleQ.Webinterface
 
                 logger.Debug("Starting schedulers");
                 jobScheduler.Start().Wait();
+                keepAlive = new KeepAlive();
+                keepAlive.Start();
                 logger.Debug("Application started successfully");
             }
             catch (Exception ex)
             {
                 logger.Fatal(ex, "Unexpected error while starting application");
+                throw ex;
+            }
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+            try
+            {
+                keepAlive?.Dispose();
+                keepAlive = null;
+
+                new KeepAlive().KeepAliveRequest();
+                logger.Debug("Performed keep-alive request to prevent shut down.");
+            }
+            catch (Exception ex)
+            {
+                logger.Fatal(ex, "Keeping alive failed");
                 throw ex;
             }
         }
@@ -61,7 +82,7 @@ namespace SimpleQ.Webinterface
                         .Where(s => s.StartDate < Literal.NextMidnight.Add(TimeSpan.FromMinutes(10)))
                         .ToList();
                     int count = 0;
-                    
+
                     foreach (var s in query)
                     {
                         var success = SurveyQueue.EnqueueSurvey(s.SvyId, s.StartDate, s.CustCode).Result;
---
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Schedulers/KeepAlive.cs
@@ -0,0 +1,78 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Web;
+
+namespace SimpleQ.Webinterface.Schedulers
+{
+    public class KeepAlive : IDisposable
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        private EventWaitHandle waitHandle = new AutoResetEvent(false);
+        private readonly object lck = new object();
+        private bool cancelled = false;
+        private string url;
+
+        public KeepAlive()
+        {
+            url = ConfigurationManager.AppSettings["SiteUrl"];
+        }
+
+        public void Start()
+        {
+            cancelled = false;
+            Thread t = new Thread(Execute);
+            t.Start();
+        }
+
+        public void KeepAliveRequest()
+        {
+            logger.Debug("Performing keep-alive request");
+            Request();
+        }
+
+        private void Request()
+        {
+            try
+            {
+                HttpClient http = new HttpClient();
+                var result = http.GetAsync(url).Result;
+                logger.Trace($"Performed GET request to {url} with status code {result.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                logger.Warn(ex, $"Request to {url} failed");
+            }
+        }
+
+        public void Stop()
+        {
+            lock (lck)
+            {
+                if (cancelled) return;
+                cancelled = true;
+                waitHandle.Set();
+            }
+        }
+
+        private void Execute()
+        {
+            while (!cancelled)
+            {
+                Request();
+                waitHandle.WaitOne(TimeSpan.FromMinutes(3), true);
+            }
+        }
+
+        #region IDisposable Members
+        public void Dispose()
+        {
+            Stop();
+        }
+        #endregion
+    }
+}
