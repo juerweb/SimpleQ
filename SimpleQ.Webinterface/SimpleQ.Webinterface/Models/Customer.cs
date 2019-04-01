d532d1314165d4d36c06b4e6a6b1c510fc1736db
Lukas Schendlinger <i14061@student.htlwrn.ac.at>
2019-04-01 14:15:08 +0200

    jsonignorer ausgeführt

--- SimpleQ.Webinterface/SimpleQ.Webinterface/Models/Customer.cs
+++ SimpleQ.Webinterface/SimpleQ.Webinterface/Models/Customer.cs
@@ -46,13 +46,23 @@ namespace SimpleQ.Webinterface.Models
         public bool Deactivated { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        [System.Web.Script.Serialization.ScriptIgnore(ApplyToOverrides = true)]
+        [Newtonsoft.Json.JsonIgnore]
         public virtual ICollection<Bill> Bills { get; set; }
+        [System.Web.Script.Serialization.ScriptIgnore(ApplyToOverrides = true)]
+        [Newtonsoft.Json.JsonIgnore]
         public virtual PaymentMethod PaymentMethod { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        [System.Web.Script.Serialization.ScriptIgnore(ApplyToOverrides = true)]
+        [Newtonsoft.Json.JsonIgnore]
         public virtual ICollection<Department> Departments { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        [System.Web.Script.Serialization.ScriptIgnore(ApplyToOverrides = true)]
+        [Newtonsoft.Json.JsonIgnore]
         public virtual ICollection<SurveyCategory> SurveyCategories { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        [System.Web.Script.Serialization.ScriptIgnore(ApplyToOverrides = true)]
+        [Newtonsoft.Json.JsonIgnore]
         public virtual ICollection<AnswerType> AnswerTypes { get; set; }
     }
 }
